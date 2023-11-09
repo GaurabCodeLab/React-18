d04a39035a92f0b513ad204c1e1f2088a99069bc
Gaurab Kumar <137804987+GaurabCodeLab@users.noreply.github.com>
2023-11-10 01:06:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, {createContext} from "react";
-import MainComponent from "./components/MainComponent";
+import Child from "./printNumber";
+import { useState, createContext } from "react";
 
-export const LoginContext = createContext();
+export const dataContext = createContext();
 function App(){
+  const [data, setData] = useState("ram");
   return (
-    <LoginContext.Provider value={"ram"}>
-      <div>
-        <MainComponent/>
-      </div>
-    </LoginContext.Provider>
+    <>
+    <h1>Hello Parent Component</h1>
+    <dataContext.Provider value={data}>
+     <Child/>
+    </dataContext.Provider>
+    <button onClick={()=>setData("sita")}>Click Me !!!</button>
+    </>
   )
 }
 
