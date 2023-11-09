b2e33f7065add4ef07114b6462550d4c0f2f96c7
Gaurab Kumar <137804987+GaurabCodeLab@users.noreply.github.com>
2023-11-10 01:06:04 +0530

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -1,1 +1,13 @@
+import { dataContext } from "./App";
+import {useContext} from 'react';
 
+function Child(){
+    const data = useContext(dataContext);
+    return (
+        <>
+        <h1>Hello child component, data is {data}</h1>
+        </>
+    )
+}
+
+export default Child;
